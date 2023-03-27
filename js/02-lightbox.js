09f1a928cf44b65ccc6691d187518d130227d244
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-03-27 19:20:55 +0200

    task02
    02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const createHtmlLi = createGalleryImage(galleryItems);
+
+console.log(gallery)
+
+function createGalleryImage(images) {
+    return images
+        .map(({ preview, original, description }) => {
+            return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}" />
+        </a>
+    </li>`
+        }
+    )
+    .join('');
+}
+
+gallery.insertAdjacentHTML('afterbegin', createHtmlLi);
+
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
+
+
